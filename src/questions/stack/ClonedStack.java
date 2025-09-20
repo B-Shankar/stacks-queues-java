4cba6ff07e23661036b8cf7b4acb3af8764f86b9
B-Shankar <learn.bhimashankar@gmail.com>
2025-09-20 22:29:22 +0530

    Clone a Stack without using extra space

--- src/questions/stack/ClonedStack.java
+++ src/questions/stack/ClonedStack.java
@@ -5,6 +5,7 @@ import java.util.*;
 //Clone a Stack without using extra space: GFG
 class ClonedStack {
 
+    //Reverse The Stack - O(n ^ 2) Time and O(1) Space
     static void clonedStack(Stack<Integer> st, Stack<Integer> cloned) {
 
         int count = 0;
@@ -31,6 +32,24 @@ class ClonedStack {
             cloned.push(st.peek());
             st.pop();
         }
+    }
+
+    //Using Recursion - O(n) Time and O(n) Space for Recursion Call Stack
+    static void clonedStackUsingRecursion(Stack<Integer> st, Stack<Integer> cloned) {
+
+        // If the source stack is empty, return.
+        if (st.empty())
+            return;
+
+        // Pop an element from the source stack.
+        int temp = st.peek();
+        st.pop();
+
+        // Recursively clone the remaining elements.
+        clonedStack(st, cloned);
+
+        // Push the popped element to the cloned stack.
+        cloned.push(temp);
     }
 
     public static void main(String[] args) {
